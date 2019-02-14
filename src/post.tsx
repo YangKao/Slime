251d988605afb308ecc7a4ad4e7702bd85677d56
Yang Keao <keao.yang@yahoo.com>
2019-02-14 17:41:32 +0800

    Add markdown Render

--- src/post.tsx
+++ src/post.tsx
@@ -2,16 +2,25 @@ import {IPost} from "./network/github"
 import {PromiseBuilder} from "./promiseBuilder/promiseBuilder";
 import {getPost} from "./network/post";
 import * as React from "react";
+import * as MarkdownIt from "markdown-it";
 
-interface Props {post: IPost}
+interface Props {
+    post: IPost
+}
+
 export function Post({post}: Props) {
     const postPromise = getPost(post.src);
 
     return <PromiseBuilder
         promise={postPromise}
-        resolveFunction={post => <div className="post">
-            {post}
-        </div>}
+        resolveFunction={
+            (post) => {
+                const md = new MarkdownIt();
+                return <div className="post">
+                    {md.render(post)}
+                </div>
+            }
+        }
         pendingFunction={() => <div className="post">
             WAITING
         </div>}
